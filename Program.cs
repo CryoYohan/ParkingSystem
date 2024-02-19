dc5e2391f956fee27318bd4c0d53b162d2bddc67
Cyril John T. Ypil <cyrilypil@gmail.com>
2024-02-19 19:16:21 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,232 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using Negus = System.Console;
+// Trapping for Null Input
+
+namespace ParkingSystem
+{
+    public abstract class Charge
+    {
+        public double chargefee = 0;
+        public string plateNo = "", vehicleType = "", vehicleBrand = "";
+        public double motorbikeFee = 20.00, suvVanFee = 40.00, sedanFee = 30.00;
+        public DateTime dateTime = DateTime.Now;
+        public abstract double chargeFee(double hours, double minutes);
+    }
+    class MotorBike : Charge
+    {
+        public override double chargeFee(double hours, double minutes)
+        {
+            if (minutes >= 30)
+            {
+                chargefee = (hours * 5.00) + motorbikeFee;
+                return chargefee + 5.00;
+            }
+            else
+            {
+                chargefee = (hours * 5.00) + motorbikeFee;
+                return chargefee;
+            }
+        }
+    }
+
+    class SUVan : Charge
+    {
+        public override double chargeFee(double hours, double minutes)
+        {
+            if (minutes >= 30)
+            {
+                chargefee = (hours * 20.00) + suvVanFee;
+                return chargefee + 20.00;
+            }
+            else
+            {
+                chargefee = (hours * 20.00) + suvVanFee;
+                return chargefee;
+            }
+        }
+    }
+
+    class Sedan : Charge
+    {
+        public override double chargeFee(double hours, double minutes)
+        {
+            if(minutes >= 30)
+            {
+                chargefee = (hours * 15.00) + sedanFee;
+                return chargefee + 15.00;
+            }
+            else
+            {
+                chargefee = (hours * 15.00) + sedanFee;
+                return chargefee;
+            }
+        }
+    }
+    class Blueprint
+    {
+        public Blueprint(string plateNo, string vehicleType, string vehicleBrand)
+        {
+            this.plateNo = plateNo;
+            this.vehicleType = vehicleType;
+            this.vehicleBrand = vehicleBrand;
+        }
+
+        public void GetPoint(out string plateNo, out string vehicleType, out string vehicleBrand)
+        {
+            plateNo = this.plateNo;
+            vehicleType = this.vehicleType;
+            vehicleBrand = this.vehicleBrand;
+        }
+
+        string plateNo = "", vehicleType = "", vehicleBrand = "";
+
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            MainClass m = new MainClass();
+            string choice = "";
+            do
+            {
+                string vehicleType = "";
+                DateTime parkingTime = DateTime.Now;
+                DateTime parkout = DateTime.Now;
+                Negus.Write("\tParking System\n\n\nPlate No.:");
+                string plateNo = Negus.ReadLine();
+                bool check = true;
+                Negus.Write("\nVehicle Type.:");
+                vehicleType = Negus.ReadLine();
+                while(check)
+                {
+                    switch (vehicleType.ToLower())
+                    {
+                        case "motorbike":
+                            check = false;
+                            break;
+                        case "suv":
+                        case "van":
+                            check = false;
+                            break;
+                        case "sedan":
+                            check = false;
+                            break;
+                        default:
+                            Negus.WriteLine("Invalid Vehicle Type");
+                            check = true;
+                            Negus.Write("\nVehicle Type.:");
+                            vehicleType = Negus.ReadLine();
+                            break;
+
+                    }
+                }
+               
+
+                Negus.Write("\nVehicle Brand:");
+                string vehicleBrand = Negus.ReadLine();
+
+                // Out Parameters
+                Blueprint bp = new Blueprint(plateNo, vehicleType, vehicleBrand);
+                string pn, vt, vb;
+                bp.GetPoint(out pn, out vt, out vb);
+
+                string parkin = parkingTime.ToString();
+                double days = 0;
+                double hours = 0;
+                double minutes = 0;
+                try {
+                    Negus.Write("\n\nEnter Date and Time of Park-out in this format\nmm/dd/yy 00:00AM/PM\n:");
+                    string input = Negus.ReadLine();
+                    parkout = DateTime.Parse(input);
+                    TimeSpan calcDate = parkout.Subtract(parkingTime);
+                    days = calcDate.Days;
+                    hours = calcDate.Hours;
+                    minutes = calcDate.Minutes;
+                    if (days < 0 || hours < 0 || minutes < 0)
+                    {
+                        Negus.WriteLine("\nInvalid Date/Time Format.Please follow these format.\nmm / dd / yy 00:00AM / PM\nex. 09 / 06 / 03 11:00PM\n\n");
+                        continue;
+                    }
+                }
+                catch(Exception e)
+                {
+                    Negus.WriteLine("\nInvalid Date/ Time Format. Please follow these format.\nmm/dd/yy 00:00AM/PM\nex. 09/06/03 11:00PM\n\n");
+                    continue;
+                }
+                double checkNega = identifyVehicleType(vt.ToLower(), (double)hours, days, minutes);
+                if(checkNega == 0) // if check is zero then switch statement detected an invalid vehicle type, thus, returning 0
+                {
+                    Negus.Write($"\nPlate No.: {pn}\nVehicle Type: {vt}\nVehicle Brand: {vb}\n\nYou've entered an invalid vehicle type. Choose from these vehicle types below:\n[Motorbike], [SUV], [Van], [Sedan]\nTry again.\n\n");
+                    continue;
+                }
+          
+                else
+                {
+                    Negus.Write($"\nPlate No.: {pn}\nVehicle Type: {vt}\nVehicle Brand: {vb}\n");
+                    Negus.Write($"\nDate/Time-in: {parkin}\n\t Out: {parkout}\nParking Time: {days} day(s) {hours} hr(s) {minutes} minute(s)\nAmount: {checkNega}"); //identify vehicle and compute charge
+                    Negus.WriteLine("\nDo another computation?(Yes/No): ");
+                    choice = Negus.ReadLine();
+                }
+
+            } while (!choice.ToLower().Equals("no"));
+
+        }
+        // Identifies vehicle and calculate charge fees with hours and days arguments
+        public static double identifyVehicleType(string vehicle, double hours, double days, double minutes)
+        {
+            MotorBike motorbike = new MotorBike();
+            SUVan suvan = new SUVan();
+            Sedan sedan = new Sedan();
+            switch (vehicle)
+            {
+                case "motorbike":
+                    if(days > 0)
+                    {
+                        if (hours == 0) // if hours == 0 then proceed to charge 1 day == 24 hours to chargeFee method
+                            return motorbike.chargeFee((days * 24), minutes);
+                        else 
+                        {
+                            double current = motorbike.chargeFee((hours + (days * 24)), minutes);
+                            return current;
+                        }
+                    }
+                    else
+                        return motorbike.chargeFee(hours, minutes);
+                    break;
+                case "suv":
+                case "van":
+                    if (days > 0)
+                    {
+                        if(hours == 0)
+                            return suvan.chargeFee((days * 24), minutes);
+                        else
+                        {
+                            double current = suvan.chargeFee((hours + (days * 24)), minutes);
+                            return current;
+                        }
+                        
+                    }
+                    else
+                        return suvan.chargeFee(hours,minutes);
+                    break;
+                case "sedan":
+                    if (days > 0)
+                    {
+                        if (hours == 0)
+                            return sedan.chargeFee((days * 24), minutes);
+                        else
+                        {
+                            double current = sedan.chargeFee((hours + (days * 24)), minutes);
+                            return current;
+                        }
+                    }
+                    else
+                        return sedan.chargeFee(hours, minutes);
+                    break;
+                default:
+                    return 0;
+                    break;
+            }
+        }
+    }
+}
